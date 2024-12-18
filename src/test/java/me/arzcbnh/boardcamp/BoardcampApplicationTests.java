c9f6b189fccd0ada1db23b3666b7b74f8723e174
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-18 04:28:54 -0300

    Initial commit

---
+++ src/test/java/me/arzcbnh/boardcamp/BoardcampApplicationTests.java
@@ -0,0 +1,13 @@
+package me.arzcbnh.boardcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
