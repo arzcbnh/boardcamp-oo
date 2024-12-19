fe4ff3e2884f1ed9f09d65e452ae6960789851e0
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-19 03:11:09 -0300

    Extend rental repository with custom query

--- src/main/java/me/arzcbnh/boardcamp/repositories/RentalRepository.java
+++ src/main/java/me/arzcbnh/boardcamp/repositories/RentalRepository.java
@@ -1,9 +1,18 @@
 package me.arzcbnh.boardcamp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import me.arzcbnh.boardcamp.models.RentalModel;
 
 @Repository
-public interface RentalRepository extends JpaRepository<RentalModel, Long> {}
+public interface RentalRepository extends JpaRepository<RentalModel, Long> {
+    @Query(
+        nativeQuery = true,
+        value = "SELECT * FROM rentals WHERE returnDate IS NOT NULL;"
+    )
+    List<RentalModel> findAllNotReturnedByGameId(Long gameId);
+}
