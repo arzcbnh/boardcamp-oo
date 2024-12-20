1fd9d0cc19ccb6fd3c4809a6c25524226b177258
Matheus Henrique <matthpmoreira@gmail.com>
2024-12-20 01:18:35 -0300

    Handle all exceptions

--- src/main/java/me/arzcbnh/boardcamp/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/me/arzcbnh/boardcamp/exceptions/GlobalExceptionsHandler.java
@@ -1,5 +1,6 @@
 package me.arzcbnh.boardcamp.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,5 +21,15 @@ public class GlobalExceptionsHandler {
     @ExceptionHandler({ RentalNotReturnedException.class })
     public ResponseEntity<String> handleRentalNotReturned(RentalNotReturnedException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler({ ConflictException.class })
+    public ResponseEntity<String> handleConflict(ConflictException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler({ DataIntegrityViolationException.class })
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException exception) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(exception.getMessage());
     }
 }
